b81d09350f419355510115405f46372c5190f892
smarataggarwal <smarataggarwal@gmail.com>
2025-11-27 17:55:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const messages = [
+    "WHY DID YOU COME HERE",
+    "XLIPTAN REMEMBERS YOU",
+    "YOU WERE NOT SUPPOSED TO SEE THIS",
+    "YOU ARE BEING OBSERVED",
+    "THIS IS NOT A WEBSITE",
+    "YOU CANNOT LEAVE NOW",
+    "THE CODE KNOWS"
+];
+
+let i = 0;
+const whisper = document.getElementById("whisper");
+
+setInterval(() => {
+    whisper.innerText = messages[i % messages.length];
+    whisper.style.opacity = 1;
+    setTimeout(() => whisper.style.opacity = 0, 2000);
+    i++;
+}, 3000);
+
+setInterval(() => {
+    document.body.style.transform = `translate(${Math.random()*6-3}px, ${Math.random()*6-3}px)`;
+    setTimeout(()=>document.body.style.transform="translate(0,0)", 100);
+}, 4000);
